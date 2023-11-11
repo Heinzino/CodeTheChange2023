6f3ee755e0f553d51727dd0497f6396fd5483246
Heinzino <51274202+Heinzino@users.noreply.github.com>
2023-11-11 14:25:20 -0700

    newUser skeleton

--- backend/dbScript.py
+++ backend/dbScript.py
@@ -1,18 +1,19 @@
 from fastapi import FastAPI, Depends, HTTPException
-from alchemyModel import get_db, Points 
+from alchemyModel import get_db, Points
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 
 app = FastAPI()
 
-@app.post("/newUser")
-def newUser():
-    pass
+@app.post("/newUser/")
+def newUser(name : str, UCID: str, hashed_password: str, db: Session = Depends(get_db)):
+    new_user = Points(name = name, UCID = UCID, hashed_password=hashed_password, points = 0)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return
 
 
-@app.post('/success')
-def updatePoints():
-    pass
 
 
