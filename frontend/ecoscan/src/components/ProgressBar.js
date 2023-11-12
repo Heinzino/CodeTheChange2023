9e547d6e6e066d5a989783660f93558dbc82f36d
Aidan <aidanhuang10@gmail.com>
2023-11-11 23:51:59 -0700

    icon pictures done

--- frontend/ecoscan/src/components/ProgressBar.js
+++ frontend/ecoscan/src/components/ProgressBar.js
@@ -8,7 +8,7 @@ const ProgressBar = ({points}) =>{
     return(
       <View style={tw`flex-row justify-between items-center`}>
         <Progress.Bar progress={0.8} width={200} height={20}/>
-        <Text>ASDFAS</Text>
+        <Text>Progress Bar</Text>
       </View>
     );
   } 
--- frontend/ecoscan/src/navigation/tabs.js
+++ frontend/ecoscan/src/navigation/tabs.js
@@ -3,13 +3,31 @@ import{ createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomePage from '../screens/HomePage';
 import CameraPage from '../screens/CameraPage';
 import RewardsPage from '../screens/RewardsPage';
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 const Tab = createBottomTabNavigator();
 
 
 const Tabs = ()=>{
     return(
-        <Tab.Navigator>
+        <Tab.Navigator screenOptions={({ route }) => ({
+            tabBarIcon: ({ color, size }) => {
+              let iconName;
+    
+              // Set the icon based on the route name
+              if (route.name === 'Home') {
+                iconName = 'home';
+              } else if (route.name === 'Camera') {
+                iconName = 'camera';
+              } else if (route.name === 'Rewards') {
+                iconName = 'gift';
+              }
+    
+              // Return the appropriate icon component
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+          })}
+        >
             <Tab.Screen name="Home" component={HomePage} options={{ headerShown: false }} />
             <Tab.Screen name="Camera" component={CameraPage} options={{ headerShown: false }}/>
             <Tab.Screen name="Rewards" component={RewardsPage} options={{ headerShown: false }}/>
