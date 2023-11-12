62676ebb96dfcbdbdf4c55ceceb4fff50bfef28a
Aidan <aidanhuang10@gmail.com>
2023-11-11 21:52:25 -0700

    fixed rewards

--- frontend/ecoscan/src/screens/RewardsPage.js
+++ frontend/ecoscan/src/screens/RewardsPage.js
@@ -1,14 +1,18 @@
+import React,{useEffect,useState} from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import {View, Text } from 'react-native';
 import Header from '../components/Header';
 
 const RewardsPage = () => {
   return (
     <View>
-    <Header name="Jun Chao" appLogo="Logo"/>
+    <Header name="Jun Chao" imageUrl="../assets/newpfp.jpg"/>
     <Text style={{ fontSize: 24, textAlign: 'center', marginTop: 20 }}>
       Rewards
     </Text>
     </View>
+
   );
 };
 
