0fe8cdaad454ef6035e62aebd9dcd71d0830790e
Heinzino <51274202+Heinzino@users.noreply.github.com>
2023-11-11 22:40:37 -0700

    api initial fast

--- frontend/ecoscan/src/screens/HomePage.js
+++ frontend/ecoscan/src/screens/HomePage.js
@@ -5,14 +5,16 @@ import {View, Text } from 'react-native';
 import Header from '../components/Header';
 import Stats from '../components/Stats';
 import ProgressBar from '../components/ProgressBar';
-
+import Pull from './exampleFetchApi';
 
 // Home screen component
 const HomePage = () => {
   return (
     <View>
+    
     <Header name="Jun Chao" imageUrl="../assets/newpfp.jpg"/>
     <Stats points="12"/>
+    <Pull/>
     <ProgressBar points="0.9"/>
     <Text style={{ fontSize: 24, textAlign: 'center', marginTop: 20 }}>Home </Text>
     </View>
--- frontend/ecoscan/src/screens/exampleFetchApi.js
+++ frontend/ecoscan/src/screens/exampleFetchApi.js
@@ -1,9 +1,69 @@
 import React, { useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { NavigationContainer,DarkTheme} from "@react-navigation/native";
-import Tabs from './src/navigation/tabs';
-import Login from './src/screens/LoginPage';
+import api from '../../api';
+import App from '../../App';
 
-<tbody>
-    {points}
-</tbody>
+const Pull =() =>{
+    
+    const [points,setPoints] = useState([]);
+    const [formData,setFormData] = useState({
+        name: '',
+        UCID: '',
+        hashed_password: '',
+        points: 0
+    });
+
+    const fetchPoints = async () => {
+        const response = await api.get('/points/');
+        setPoints(response.data)
+    };
+
+    useEffect(() => {
+        fetchPoints();
+    }, []);
+
+    const handleInputChange = (event) => {
+        const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+        setFormData({
+        ...formData,
+        [event.target.name] : value,
+        });
+    };
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault(); //makes us only control submitting the form
+        await api.post('/points/',formData);
+        fetchPoints();
+        setFormData({
+        name: '',
+        UCID: '',
+        hashed_password: '',
+        points: 0
+        });
+  };
+
+
+    return(
+      
+    <table>
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Points</th>
+        </tr>
+    </thead>
+    <tbody>
+        {points.map((points) => (
+            <tr key={points.id}>
+                <td>{points.name}</td>
+                <td>{points.points}</td>
+            </tr>
+        ))}
+    </tbody>
+    </table>
+    );
+
+}
+
+export default Pull;
